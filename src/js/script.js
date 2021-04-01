2d29ed14be11c64c21e343cc5b6bd740dd905d78
Mishelinus <mishelin06@yandex.ru>
2021-04-01 23:21:32 +0300

    added section carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,23 @@
-window.addEventListener('DOMContentLoaded', () => {
-   const menu = document.querySelector('.menu'),
-   menuItem = document.querySelectorAll('.menu__item'),
-   hamburger = document.querySelector('.hamburger');
 
-   hamburger.addEventListener('click', () => {
-       hamburger.classList.toggle('hamburger__active');
-       menu.classList.toggle('menu__active');
-   });
 
-   menuItem.forEach(item => {
-       item.addEventListener('click', () => {
-           hamburger.classList.toggle('hamburger__active');
-           menu.classList.toggle('menu__active');
-       })
-   })
-})
+   // start carousel
+
+   $(document).ready(function(){
+    $('.carousel__inner').slick({
+      dots: true,
+
+    });
+  });
+
+// end carousel
+
+// responsive: [
+//   {
+//     breakpoint: 992,
+//     settings: {
+//         dots: true,
+//       arrows: false,
+      
+//     }
+//   }
+// ]
