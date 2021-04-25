5a3c2001e38d318b7cd83819a3994502af61866f
Serov Mihail <mishelin06@yandex.ru>
2021-04-25 23:44:36 +0300

    Update smart.php

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'mishelin06@yandex.ru';                 // Наш логин
-$mail->Password = 'm09101985m';                           // Наш пароль от ящика
+$mail->Username = '*******';                 // Наш логин
+$mail->Password = '*******';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('mishelin06@yandex.ua', 'RunSmart');   // От кого письмо 
-$mail->addAddress('mishelin06@yandex.ru','Михаил Серов');     // Add a recipient
+$mail->setFrom('*******', 'RunSmart');   // От кого письмо 
+$mail->addAddress('*******','Mishelin');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
